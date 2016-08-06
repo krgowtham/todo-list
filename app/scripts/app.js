2e6884a44ddcfcb3be5ebd697c7180d93bc0fca4
gowtham <gowtham1206@gmail.com>
2016-08-06 16:42:27 +0530

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gowthamApp
+ * @description
+ * # gowthamApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gowthamApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gowthamApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gowthamApp
+ */
+angular.module('gowthamApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
